94666b9a0164a5b1f45478fe6c1952d59ea439f7
Andry Pebrianto <andrypeb227@gmail.com>
2024-02-23 20:08:08 +0700

    lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const router = createRouter({
       name: "aboutPage",
       component: AboutViewVue,
     },
+    {
+      path: "/movies",
+      name: "moviesPage",
+      component: () => import("../views/MoviesView.vue"),
+    },
   ],
 });
 
